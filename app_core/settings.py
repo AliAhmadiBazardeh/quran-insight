1a84258eeba24756a5a38b7444e6a72e84302a23
AliAhmadiBazardeh <a.ahmadi768276@gmail.com>
2026-04-23 13:29:26 +0330

    update board and allow host * in settings.py

--- app_core/settings.py
+++ app_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ne@4sxkhi_%a+-abx^qgf_ttund!la4xxi7))htvta*y=!e3ye
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
